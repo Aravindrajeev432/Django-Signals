57bd2ec42930711f9b320a370cf5a83cf2c0e569
Aravind R <ndarvi48332@gmail.com>
2022-11-25 18:31:35 +0530

    second

--- postandpre/myapi/signals.py
+++ postandpre/myapi/signals.py
@@ -5,9 +5,13 @@ from .models import Store
 # pre save
 @receiver(signals.pre_save ,sender=Store)
 def quantitychecker(sender, instance, using, **kwargs):
-    product = Store.objects.get(id=instance.id)
 
-    if instance.quantity > product.quantity:
-        raise Exception("Out of Stock")
+    try:
+        product = Store.objects.get(id=instance.id)
+
+        if instance.quantity > product.quantity:
+            raise Exception("Out of Stock")
+    except Store.DoesNotExist:
+        pass
 
 # visit apps.py line number 8 ! important
--- postandpre/postandpre/settings.py
+++ postandpre/postandpre/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'd8v)hssj1y-w+pg%ap3^cgatfr@q#p)ki&^9&+azxm!tdjfo&n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
